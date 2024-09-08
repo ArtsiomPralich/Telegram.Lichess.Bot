4a02f82f711315c4bf2d77305d9d881bbc50a9eb
HP <HP@DESKTOP-MHNHM2E>
2024-09-08 13:02:50 +0300

    Добавьте файлы проекта.

---
+++ Telegram.Lichess.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Telegram.Lichess.Bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
