f151233cf9cccdeba27bbdc62f73506733d1b260
Artsiom Pralich <artsiom.pralich@gmail.com>
2024-09-08 23:05:43 +0300

    Add  a simple script for receiving messages

--- Telegram.Lichess.Bot/Program.cs
+++ Telegram.Lichess.Bot/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace Telegram.Lichess.Bot
+﻿using Telegram.Bot.Polling;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+namespace Telegram.Lichess.Bot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static ITelegramBotClient _botClient;
+        private static ReceiverOptions _receiverOptions;
+
+        static async Task Main(string[] args)
         {
-            
+            _botClient = new TelegramBotClient("");
+            _receiverOptions = new ReceiverOptions 
+            {
+                AllowedUpdates = new[] //https://core.telegram.org/bots/api#update
+                {
+                    UpdateType.Message, 
+                },
+                ThrowPendingUpdates = true,
+            };
+
+            using var cts = new CancellationTokenSource();
+            _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token);
+
+            var me = await _botClient.GetMeAsync();
+            Console.WriteLine($"{me.FirstName} is running!");
+
+            await Task.Delay(-1);
+        }
+
+        private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            try
+            {
+                switch (update.Type)
+                {
+                    case UpdateType.Message:
+                        {
+                            Console.WriteLine("Message received!");
+                            return;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = error switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => error.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
         }
     }
 }
