d80b83eca5c4958e4fe2aef31887a6589f68c99d
Artsiom Pralich <artsiom.pralich@gmail.com>
2024-09-08 13:04:58 +0300

    Initial develop branch

--- Telegram.Lichess.Bot/Program.cs
+++ Telegram.Lichess.Bot/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace Telegram.Lichess.Bot
+﻿namespace Telegram.Lichess.Bot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
